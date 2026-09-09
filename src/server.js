170a6f19a4645e01fa3160ae59f98c2428153250
Dmytro <dlmakseverin@gmail.com>
2026-09-09 18:22:13 +0300

    add pino-http logger

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import pino from 'pino-http';
 
 dotenv.config();
 
@@ -9,6 +10,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
