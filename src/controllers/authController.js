3c48b1533e3b6738a17f4de4913d295dcad8057b
Dmytro <dlmakseverin@gmail.com>
2026-09-09 19:53:27 +0300

    add user registration

---
+++ src/controllers/authController.js
@@ -0,0 +1,24 @@
+import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
+import { User } from '../models/user.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
+
+export const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw createHttpError(400, 'Email in use');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  const session = await createSession(user._id);
+  setSessionCookies(res, session);
+
+  res.status(201).json(user);
+};
