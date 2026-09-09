63004fad6a9bf7caad37157ee7ebd37d151a7a1f
Dmytro <dlmakseverin@gmail.com>
2026-09-09 20:12:41 +0300

    add PATCH /users/me/avatar

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { errors } from 'celebrate';
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.use(cors());
 app.use(cookieParser());
 
 app.use(authRoutes);
+app.use(userRoutes);
 app.use(notesRoutes);
 app.use(errors());
 
