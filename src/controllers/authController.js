667b54e5a633a27415c92650dab49c8095fec885
Dmytro <dlmakseverin@gmail.com>
2026-09-09 19:53:55 +0300

    add user login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 import { User } from '../models/user.js';
+import { Session } from '../models/session.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 
 export const registerUser = async (req, res) => {
@@ -21,4 +22,26 @@ export const registerUser = async (req, res) => {
   setSessionCookies(res, session);
 
   res.status(201).json(user);
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const session = await createSession(user._id);
+  setSessionCookies(res, session);
+
+  res.status(200).json(user);
 };
