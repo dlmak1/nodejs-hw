744dd2b441d3a7806009368c96333d0f7ac8dba5
Dmytro <dlmakseverin@gmail.com>
2026-09-09 20:12:41 +0300

    add cloudinary upload helper

---
+++ src/utils/saveFileToCloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+export const saveFileToCloudinary = (buffer, userId) => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        folder: 'notes-avatars',
+        public_id: String(userId),
+        overwrite: true,
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+
+        resolve(result);
+      },
+    );
+
+    stream.end(buffer);
+  });
+};
