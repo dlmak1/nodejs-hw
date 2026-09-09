53ef5da79ac3b0906db1efd4fa5c150809939235
Dmytro <dlmakseverin@gmail.com>
2026-09-09 19:42:06 +0300

    handle celebrate validation errors

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { errors } from 'celebrate';
 import notesRoutes from './routes/notesRoutes.js';
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(notesRoutes);
+app.use(errors());
 
 app.use(notFoundHandler);
 app.use(errorHandler);
