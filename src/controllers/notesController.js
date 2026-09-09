88ef75f01f00b05bd3d19bd54b2fd267f829f4a5
Dmytro <dlmakseverin@gmail.com>
2026-09-09 18:55:55 +0300

    add GET /notes and GET /notes/:noteId

---
+++ src/controllers/notesController.js
@@ -0,0 +1,18 @@
+import createHttpError from 'http-errors';
+import { Note } from '../models/note.js';
+
+export const getAllNotes = async (req, res) => {
+  const notes = await Note.find();
+  res.status(200).json(notes);
+};
+
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
