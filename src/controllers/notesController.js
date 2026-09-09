83bcf8b91eb8252e01a24d132320f99eaae1643a
Dmytro <dlmakseverin@gmail.com>
2026-09-09 18:56:21 +0300

    add DELETE and PATCH /notes/:noteId

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -21,3 +21,27 @@ export const createNote = async (req, res) => {
   const note = await Note.create(req.body);
   res.status(201).json(note);
 };
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findByIdAndDelete(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findByIdAndUpdate(noteId, req.body, {
+    new: true,
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -3,6 +3,8 @@ import {
   getAllNotes,
   getNoteById,
   createNote,
+  deleteNote,
+  updateNote,
 } from '../controllers/notesController.js';
 
 const router = Router();
@@ -10,5 +12,7 @@ const router = Router();
 router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
 router.post('/notes', createNote);
+router.delete('/notes/:noteId', deleteNote);
+router.patch('/notes/:noteId', updateNote);
 
 export default router;
