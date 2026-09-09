dd1653fc6853a3b99c5f8e4fda51f22a3b3f5590
Dmytro <dlmakseverin@gmail.com>
2026-09-09 18:22:34 +0300

    add GET /notes and GET /notes/:noteId

--- src/server.js
+++ src/server.js
@@ -18,6 +18,20 @@ app.use(
   }),
 );
 
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
